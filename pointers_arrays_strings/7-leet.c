475d2a063960a884da2f3ef8955a6524c4e274ab
ab10680 <10680@holbertonstudents.com>
2025-02-27 18:17:53 +0000

    task 7 update 2

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,28 +1,27 @@
 #include "main.h"
 
 /**
- * leet - Encodes a string into 1337 (leet speak).
- * @str: The string to be encoded.
- *
- * Return: A pointer to the encoded string.
+ * leet - Encodes a string into 1337
+ * @str: The input string
+ * Return: Pointer to the modified string
  */
 char *leet(char *str)
 {
 char *ptr = str;
-char *leet_chars = "74031"; /* Corresponding replacements for T, A, E, O, L */
-char letters[] = "taeolTAEOL"; /* Letters to be replaced */
-int i; /* Declare 'i' here */
-while (*ptr)
+char letters[] = "aAeEoOtTlL"; /* Letters to be replaced */
+char leet_nums[] = "4433007711"; /* 4= A/a, 3= E/e, 0= O/o, 7= T/t, 1= L/l */
+int i;
+while (*ptr) /* Outer loop to iterate over the string */
 {
 for (i = 0; letters[i]; i++)
 {
 if (*ptr == letters[i])
 {
-*ptr = leet_chars[i / 2]; /* Replace with the corresponding leet character */
+*ptr = leet_nums[i];
 break; /* Exit the for loop if a replacement was made */
 }
 }
 ptr++;
 }
-return (str); /* Added parentheses around the return value */
+return (str);
 }
