33b37c0cf7196f16e31ea8c91a2bcffc481f7426
ab10680 <10680@holbertonstudents.com>
2025-03-11 22:37:40 +0000

    task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure for a dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Description: This structure defines dog with its name, age, & owner
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif /* DOG_H */
