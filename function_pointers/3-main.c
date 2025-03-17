f19daff60e5c0511c75c338caebd581da1c7620c
ab10680 <10680@holbertonstudents.com>
2025-03-17 21:17:53 +0000

    task 3 update

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -6,24 +6,35 @@ int main(int argc, char *argv[])
 {
 int num1, num2;
 int (*operation)(int, int);
-if (argc != 4)  /*  Check for the correct number of arguments */
+/* Check for the correct number of arguments */
+if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
-num1 = atoi(argv[1]);  /* Convert the first argument to an integer */
-num2 = atoi(argv[3]);  /* Convert the second argument to an integer */
-operation = get_op_func(argv[2]);  /* Get the operation function */
-if (operation == NULL)  /* Check if the operation is valid */
+/* Convert the first and third arguments to integers */
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+/* Ensure the operator argument is a single character */
+if (argv[2][1] != '\0')
 {
 printf("Error\n");
 exit(99);
 }
-if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)  /* Check for division by zero */
+operation = get_op_func(argv[2]); /* Get the operation function */
+/* Check if the operation is valid */
+if (operation == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+/* Check for division by zero */
+if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
 {
 printf("Error\n");
 exit(100);
 }
-printf("%d\n", operation(num1, num2));  /* Perform the operation and print the result */
+/* Perform the operation and print the result */
+printf("%d\n", operation(num1, num2));
 return (0);
 }
