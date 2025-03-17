0e99563a125467f306b30f3c920259cef85ee087
ab10680 <10680@holbertonstudents.com>
2025-03-17 20:05:48 +0000

    task 1

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -5,5 +5,6 @@ void print_name(char *name, void (*f)(char *));
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
 int _putchar(char c);
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
