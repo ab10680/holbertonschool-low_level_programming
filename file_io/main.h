83800dc0d075e9f51fbe3263fe6d97f67791f384
ab10680 <10680@holbertonstudents.com>
2025-04-05 22:39:47 +0000

    task 3 - 06-04-2025 | 01:39

--- file_io/main.h
+++ file_io/main.h
@@ -1,10 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function Prototypes */
-int open_source(char *file);
-int open_dest(char *file);
+int open_source(const char *file);
+int open_dest(const char *file);
 void copy_content(int fd_from, int fd_to, char *src, char *dest);
 void close_fd(int fd);
 
-#endif /* MAIN_H */
+#endif
