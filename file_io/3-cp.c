5fbc2a27c6c31b651bf1a75393d2d4f3ebc70530
ab10680 <10680@holbertonstudents.com>
2025-04-05 22:06:49 +0000

    task 3 - 06-04-2025 | 01:06

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,88 +1,89 @@
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <math.h>
+#include <unistd.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /**
- * open_source - Open the source file for reading.
- * @file: Name of source file.
- * Return: File descriptor, or exit(98) on failure.
+ * open_source - Opens the source file for reading.
+ * @file: Name of the source file.
+ *
+ * Return: File descriptor of the opened source file.
+ *         Exits with code 98 on failure.
  */
 int open_source(char *file)
 {
   int fd;
 
   fd = open(file, O_RDONLY);
-  if (fd < 0)
+  if (fd == -1)
     {
       dprintf(STDERR_FILENO,
-	      "Error: Can't read from file %s\n",
-	      file);
+	      "Error: Can't read from file %s\n", file);
       exit(98);
     }
   return (fd);
 }
 
 /**
- * open_dest - Open or create the destination file for writing.
- * @file: Name of destination file.
- * Return: File descriptor, or exit(99) on failure.
+ * open_dest - Opens/creates the destination file for writing.
+ * @file: Name of the destination file.
+ *
+ * Return: File descriptor of the opened destination file.
+ *         Exits with code 99 on failure.
  */
 int open_dest(char *file)
 {
   int fd;
 
   fd = open(file, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-  if (fd < 0)
+  if (fd == -1)
     {
       dprintf(STDERR_FILENO,
-	      "Error: Can't write to %s\n",
-	      file);
+	      "Error: Can't write to %s\n", file);
       exit(99);
     }
   return (fd);
 }
 
 /**
- * copy_content - Reads data from fd_from and writes it to fd_to.
+ * copy_content - Copies content from one file descriptor to another.
  * @fd_from: Source file descriptor.
  * @fd_to: Destination file descriptor.
- * @src: Source filename (for error messages).
- * @dest: Destination filename (for error messages).
- *
- * Exits(98) if read() fails, Exits(99) if write() fails.
+ * @src: Source file name (for error messages).
+ * @dest: Destination file name (for error messages).
  */
 void copy_content(int fd_from, int fd_to, char *src, char *dest)
 {
-  ssize_t rd, wr;
+  ssize_t rd = 0, wr = 0;
   char buffer[1024];
 
   while ((rd = read(fd_from, buffer, 1024)) > 0)
     {
-
-
-
       wr = write(fd_to, buffer, rd);
-      if (wr < 0 || wr != rd)
+      if (wr == -1)
 	{
 	  dprintf(STDERR_FILENO,
-		  "Error: Can't write to %s\n",
-		  dest);
+		  "Error: Can't write to %s\n", dest);
 	  close(fd_from);
 	  close(fd_to);
 	  exit(99);
 	}
-
-
-
+      if (wr != rd)
+	{
+	  dprintf(STDERR_FILENO,
+		  "Error: Can't read from file %s\n", src);
+	  close(fd_from);
+	  close(fd_to);
+	  exit(98);
+	}
     }
-  if (rd < 0)
+
+  if (rd == -1)
     {
       dprintf(STDERR_FILENO,
-	      "Error: Can't read from file %s\n",
-	      src);
+	      "Error: Can't read from file %s\n", src);
       close(fd_from);
       close(fd_to);
       exit(98);
@@ -90,27 +91,29 @@ void copy_content(int fd_from, int fd_to, char *src, char *dest)
 }
 
 /**
- * close_fd - Closes a file descriptor; exit(100) on error.
+ * close_fd - Closes a file descriptor with error checking.
  * @fd: File descriptor to close.
  */
 void close_fd(int fd)
 {
-  if (close(fd) < 0)
+  if (close(fd) == -1)
     {
       dprintf(STDERR_FILENO,
-	      "Error: Can't close fd %d\n",
-	      fd);
+	      "Error: Can't close fd %d\n", fd);
       exit(100);
     }
 }
 
 /**
- * main - Copies content of one file to another.
- * @ac: Argument count (must be 3).
- * @av: Array of arguments: cp file_from file_to
+ * main - Entry point. Copies content from one file to another.
+ * @ac: Argument count.
+ * @av: Argument vector.
  *
- * Return: 0 on success, or exit codes on various failures:
- *  97 (usage), 98 (read error), 99 (write error), 100 (close error).
+ * Return: 0 on success. Exits with various codes on failure:
+ *  97 - incorrect usage
+ *  98 - read error
+ *  99 - write error
+ *  100 - close error
  */
 int main(int ac, char *av[])
 {
@@ -122,10 +125,12 @@ int main(int ac, char *av[])
 	      "Usage: cp file_from file_to\n");
       exit(97);
     }
+
   fd_from = open_source(av[1]);
   fd_to = open_dest(av[2]);
   copy_content(fd_from, fd_to, av[1], av[2]);
   close_fd(fd_from);
   close_fd(fd_to);
+
   return (0);
 }
