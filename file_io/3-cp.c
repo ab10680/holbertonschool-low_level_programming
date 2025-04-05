f68413ee6fb906fd911ab7f4bdc6f749c78ed1aa
ab10680 <10680@holbertonstudents.com>
2025-04-05 22:23:10 +0000

    task 3 - 06-04-2025 | 01:23

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,6 +4,8 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include "main.h"
+
 
 /**
  * open_source - Opens the source file for reading.
@@ -12,15 +14,14 @@
  */
 int open_source(char *file)
 {
-  int fd = open(file, O_RDONLY);
-
-  if (fd == -1)
-    {
-      dprintf(STDERR_FILENO,
-	      "Error: Can't read from file %s\n", file);
-      exit(98);
-    }
-  return (fd);
+int fd = open(file, O_RDONLY);
+if (fd == -1)
+{
+dprintf(STDERR_FILENO,
+"Error: Can't read from file %s\n", file);
+exit(98);
+}
+return (fd);
 }
 
 /**
@@ -30,15 +31,14 @@ int open_source(char *file)
  */
 int open_dest(char *file)
 {
-  int fd = open(file, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
-  if (fd == -1)
-    {
-      dprintf(STDERR_FILENO,
-	      "Error: Can't write to %s\n", file);
-      exit(99);
-    }
-  return (fd);
+int fd = open(file, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+if (fd == -1)
+{
+dprintf(STDERR_FILENO,
+"Error: Can't write to %s\n", file);
+exit(99);
+}
+return (fd);
 }
 
 /**
@@ -50,29 +50,28 @@ int open_dest(char *file)
  */
 void copy_content(int fd_from, int fd_to, char *src, char *dest)
 {
-  ssize_t rd, wr;
-  char buffer[1024];
-
-  while ((rd = read(fd_from, buffer, 1024)) != 0)
-    {
-      if (rd == -1)
-	{
-	  dprintf(STDERR_FILENO,
-		  "Error: Can't read from file %s\n", src);
-	  close(fd_from);
-	  close(fd_to);
-	  exit(98);
-	}
-      wr = write(fd_to, buffer, rd);
-      if (wr == -1 || wr != rd)
-	{
-	  dprintf(STDERR_FILENO,
-		  "Error: Can't write to %s\n", dest);
-	  close(fd_from);
-	  close(fd_to);
-	  exit(99);
-	}
-    }
+ssize_t rd, wr;
+char buffer[1024];
+while ((rd = read(fd_from, buffer, 1024)) != 0)
+{
+if (rd == -1)
+{
+dprintf(STDERR_FILENO,
+"Error: Can't read from file %s\n", src);
+close(fd_from);
+close(fd_to);
+exit(98);
+}
+wr = write(fd_to, buffer, rd);
+if (wr == -1 || wr != rd)
+{
+dprintf(STDERR_FILENO,
+"Error: Can't write to %s\n", dest);
+close(fd_from);
+close(fd_to);
+exit(99);
+}
+}
 }
 
 /**
@@ -81,12 +80,12 @@ void copy_content(int fd_from, int fd_to, char *src, char *dest)
  */
 void close_fd(int fd)
 {
-  if (close(fd) == -1)
-    {
-      dprintf(STDERR_FILENO,
-	      "Error: Can't close fd %d\n", fd);
-      exit(100);
-    }
+if (close(fd) == -1)
+{
+dprintf(STDERR_FILENO,
+"Error: Can't close fd %d\n", fd);
+exit(100);
+}
 }
 
 /**
@@ -97,22 +96,17 @@ void close_fd(int fd)
  */
 int main(int ac, char *av[])
 {
-  int fd_from, fd_to;
-
-  if (ac != 3)
-    {
-      dprintf(STDERR_FILENO,
-	      "Usage: cp file_from file_to\n");
-      exit(97);
-    }
-
-  fd_from = open_source(av[1]);
-  fd_to = open_dest(av[2]);
-
-  copy_content(fd_from, fd_to, av[1], av[2]);
-
-  close_fd(fd_from);
-  close_fd(fd_to);
-
-  return (0);
+int fd_from, fd_to;
+if (ac != 3)
+{
+dprintf(STDERR_FILENO,
+"Usage: cp file_from file_to\n");
+exit(97);
+}
+fd_from = open_source(av[1]);
+fd_to = open_dest(av[2]);
+copy_content(fd_from, fd_to, av[1], av[2]);
+close_fd(fd_from);
+close_fd(fd_to);
+return (0);
 }
