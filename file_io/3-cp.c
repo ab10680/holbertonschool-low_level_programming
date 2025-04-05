c3c006d5cf297c5c3c49d7cb3cb63ab775877ddc
ab10680 <10680@holbertonstudents.com>
2025-04-05 21:53:46 +0000

    task 3 - 06-04-2025 | 12:53

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,9 +1,9 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
+#include <math.h>
 #include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 
 /**
  * open_source - Open the source file for reading.
@@ -12,16 +12,17 @@
  */
 int open_source(char *file)
 {
-int fd;
-fd = open(file, O_RDONLY);
-if (fd < 0)
-{
-dprintf(STDERR_FILENO,
-"Error: Can't read from file %s\n",
-file);
-exit(98);
-}
-return (fd);
+  int fd;
+
+  fd = open(file, O_RDONLY);
+  if (fd < 0)
+    {
+      dprintf(STDERR_FILENO,
+	      "Error: Can't read from file %s\n",
+	      file);
+      exit(98);
+    }
+  return (fd);
 }
 
 /**
@@ -31,16 +32,17 @@ return (fd);
  */
 int open_dest(char *file)
 {
-int fd;
-fd = open(file, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-if (fd < 0)
-{
-dprintf(STDERR_FILENO,
-"Error: Can't write to %s\n",
-file);
-exit(99);
-}
-return (fd);
+  int fd;
+
+  fd = open(file, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+  if (fd < 0)
+    {
+      dprintf(STDERR_FILENO,
+	      "Error: Can't write to %s\n",
+	      file);
+      exit(99);
+    }
+  return (fd);
 }
 
 /**
@@ -54,38 +56,37 @@ return (fd);
  */
 void copy_content(int fd_from, int fd_to, char *src, char *dest)
 {
-ssize_t rd, wr = -1;
-char buffer[1024];
-while ((rd = read(fd_from, buffer, 1024)) > 0)
-{
-if (wr < 0)
-{
-dprintf(STDERR_FILENO,
-"Error: Can't write to %s\n",
-dest);
-close(fd_from);
-close(fd_to);
-exit(99);
-}
-if (wr != rd)
-{
-dprintf(STDERR_FILENO,
-"Error: Can't read from file %s\n",
-src);
-close(fd_from);
-close(fd_to);
-exit(98);
-}
-}
-if (rd < 0)
-{
-dprintf(STDERR_FILENO,
-"Error: Can't read from file %s\n",
-src);
-close(fd_from);
-close(fd_to);
-exit(98);
-}
+  ssize_t rd, wr;
+  char buffer[1024];
+
+  while ((rd = read(fd_from, buffer, 1024)) > 0)
+    {
+
+
+
+      wr = write(fd_to, buffer, rd);
+      if (wr < 0 || wr != rd)
+	{
+	  dprintf(STDERR_FILENO,
+		  "Error: Can't write to %s\n",
+		  dest);
+	  close(fd_from);
+	  close(fd_to);
+	  exit(99);
+	}
+
+
+
+    }
+  if (rd < 0)
+    {
+      dprintf(STDERR_FILENO,
+	      "Error: Can't read from file %s\n",
+	      src);
+      close(fd_from);
+      close(fd_to);
+      exit(98);
+    }
 }
 
 /**
@@ -94,13 +95,13 @@ exit(98);
  */
 void close_fd(int fd)
 {
-if (close(fd) < 0)
-{
-dprintf(STDERR_FILENO,
-"Error: Can't close fd %d\n",
-fd);
-exit(100);
-}
+  if (close(fd) < 0)
+    {
+      dprintf(STDERR_FILENO,
+	      "Error: Can't close fd %d\n",
+	      fd);
+      exit(100);
+    }
 }
 
 /**
@@ -113,17 +114,18 @@ exit(100);
  */
 int main(int ac, char *av[])
 {
-int fd_from, fd_to;
-if (ac != 3)
-{
-dprintf(STDERR_FILENO,
-"Usage: cp file_from file_to\n");
-exit(97);
-}
-fd_from = open_source(av[1]);
-fd_to = open_dest(av[2]);
-copy_content(fd_from, fd_to, av[1], av[2]);
-close_fd(fd_from);
-close_fd(fd_to);
-return (0);
+  int fd_from, fd_to;
+
+  if (ac != 3)
+    {
+      dprintf(STDERR_FILENO,
+	      "Usage: cp file_from file_to\n");
+      exit(97);
+    }
+  fd_from = open_source(av[1]);
+  fd_to = open_dest(av[2]);
+  copy_content(fd_from, fd_to, av[1], av[2]);
+  close_fd(fd_from);
+  close_fd(fd_to);
+  return (0);
 }
