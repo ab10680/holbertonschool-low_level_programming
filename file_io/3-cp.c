5ed6912281fda88b4baa1d6ce4abb28ae40f6275
ab10680 <10680@holbertonstudents.com>
2025-04-05 22:50:56 +0000

    task 3 - 06-04-2025 | 01:50

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,115 +5,41 @@
 #include <stdio.h>
 
 /**
- * open_source - Opens the source file for reading.
- * @file: The name of the source file.
- * Return: File descriptor on success, exits with code 98 on failure.
+ * create_buffer - function that allocates 1024 bytes for a buffer.
+ * @file: pointer to the file buffer is storing chars for.
+ *
+ * Return: A pointer to the newly-allocated buffer.
  */
-int open_source(const char *file)
+char *create_buffer(char *file)
 {
-  int fd = open(file, O_RDONLY);
-
-  if (fd < 0)
+  char *buffer;
+  buffer = malloc(sizeof(char) * 1024);
+  if (!buffer)
     {
-      dprintf(STDERR_FILENO,
-	      "Error: Can't read from file %s\n", file);
-      exit(98);
-    }
-  return (fd);
-}
-
-/**
- * open_dest - Opens the destination file for writing.
- * @file: The name of the destination file.
- * Return: File descriptor on success, exits with code 99 on failure.
- */
-int open_dest(const char *file)
-{
-  int fd = open(file, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
-  if (fd < 0)
-    {
-      dprintf(STDERR_FILENO,
-	      "Error: Can't write to %s\n", file);
+      dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
       exit(99);
     }
-  return (fd);
+  return (buffer);
 }
 
 /**
- * copy_content - Copies content from source file to destination file.
- * @fd_from: Source file descriptor.
- * @fd_to: Destination file descriptor.
- * @src: Source file name.
- * @dest: Destination file name.
+ * close_file - function that closes file descriptors.
+ * @file_descriptor: file descriptor to be closed.
  */
-
-void copy_content(int fd_from, int fd_to, char *src, char *dest)
+void close_file(int file_descriptor)
 {
-  char buffer[1024];
-  ssize_t rd, wr;
-
-  while ((rd = read(fd_from, buffer, 1024)) != 0)
+  int cclose;
+  cclose = close(file_descriptor);
+  if (cclose == -1)
     {
-      if (rd == -1)
-	{
-	  dprintf(STDERR_FILENO,
-		  "Error: Can't read from file %s\n", src);
-	  close_fd(fd_from);
-	  close_fd(fd_to);
-	  exit(98);
-	}
-
-      wr = write(fd_to, buffer, rd);
-      if (wr == -1 || wr != rd)
-	{
-	  dprintf(STDERR_FILENO,
-		  "Error: Can't write to %s\n", dest);
-	  close_fd(fd_from);
-	  close_fd(fd_to);
-	  exit(99);
-	}
-    }
-}
-
-/**
- * close_fd - Closes a file descriptor and handles errors.
- * @fd: File descriptor to close.
- */
-void close_fd(int fd)
-{
-  if (close(fd) < 0)
-    {
-      dprintf(STDERR_FILENO,
-	      "Error: Can't close fd %d\n", fd);
+      dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_descriptor);
       exit(100);
     }
 }
-
 /**
- * main - Entry point for the file copy program.
- * @argc: Number of command line arguments.
- * @argv: Array of command line arguments.
- * Return: 0 on success, exits with appropriate code on failure.
+ * main - copies the contents of a file to another file.
+ * @argc: number of arguments supplied to the program.
+ * @argv: an array of character pointers listing all the arguments.
+ *
+ * Return: 0 on success.
  */
-int main(int argc, char *argv[])
-{
-  int fd_from, fd_to;
-
-  if (argc != 3)
-    {
-      dprintf(STDERR_FILENO,
-	      "Usage: cp file_from file_to\n");
-      exit(97);
-    }
-
-  fd_from = open_source(argv[1]);
-  fd_to = open_dest(argv[2]);
-
-  copy_content(fd_from, fd_to, argv[1], argv[2]);
-
-  close_fd(fd_from);
-  close_fd(fd_to);
-
-  return (0);
-}
